e3feffa518294a4d6f4e8cea879438e06bd1c767
Prudhvi Raj Vavilapalli <prudv.raj06@gmail.com>
2023-03-27 21:44:59 +0530

    added modelmapper dependency, created EmployeeDto class

---
+++ src/main/java/com/JdbcMssql/EmployeeCRUD/Dto/EmployeeDto.java
@@ -0,0 +1,16 @@
+package com.JdbcMssql.EmployeeCRUD.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+    private int id;
+    private String name;
+    private String email;
+}
